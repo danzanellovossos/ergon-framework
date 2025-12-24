4aa52373e73cbb0d077fe83df6d929dcd59898d0
Daniel Vossos <daniel.vossos@ergondata.com.br>
2025-12-24 15:09:07 -0300

    fix: update bootstrap imports

--- sdks/python/src/ergon/bootstrap/src/__project__/tasks/settings.py
+++ sdks/python/src/ergon/bootstrap/src/__project__/tasks/settings.py
@@ -1,8 +1,5 @@
 # Uncomment below to use framework imports
 # ----------------------------------------
 
-# from ergon_framework import connector
-# from ergon_framework.telemetry import logging, metrics, tracing
-
-# Utils
-# from ergon_framework.utils import load_env
+# from ergon import service, connector, telemetry
+#from ergon.utils import load_env
