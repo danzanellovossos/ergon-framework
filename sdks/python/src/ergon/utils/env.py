2ade94bdd350d9af08c80f126d6124f4daffef15
anzavossos <daniel.vossos@ergondata.com.br>
2026-01-16 12:44:52 -0300

    Refactor/execute multithread (#15)
    * refactor: executors and async consumer
    
    * refactor: executors and async consumer
    
    * refactor: load_env

--- sdks/python/src/ergon/utils/env.py
+++ sdks/python/src/ergon/utils/env.py
@@ -1,8 +1,17 @@
 import os
-
 from dotenv import load_dotenv
 
 
 def load_env():
-    env = os.environ.get("ENV", "local")
-    load_dotenv(f".env.{env}", encoding="utf-8")
+    env_file = os.environ.get("ENV_FILE")
+
+    if not env_file:
+        raise ValueError(
+            "ENV_FILE is not set. You must set the ENV_FILE environment variable."
+            "Example: ENV_FILE=.env.local"
+            "Example: ENV_FILE=.env.dev"
+            "Example: ENV_FILE=.env.prod"
+            "Example: ENV_FILE=.env.staging"
+        )
+
+    load_dotenv(env_file, encoding="utf-8", override=False)
